e93a849274d90e71a08afedc1d65c63053aaeb6c
Tomasz Zarna <tomasz.zarna@tasktop.com>
2013-08-05 16:34:25 +0200

    fix testOpenNewBugzillaTask performance test
    Change-Id: I29617badc8d5ece0b5438e9b65e20d4c241c96d5
    Signed-off-by: Tomasz Zarna <tomasz.zarna@tasktop.com>

--- org.eclipse.mylyn.tests.performance/src/org/eclipse/mylyn/tasks/tests/performance/TaskEditorPerformanceTest.java
+++ org.eclipse.mylyn.tests.performance/src/org/eclipse/mylyn/tasks/tests/performance/TaskEditorPerformanceTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Tasktop Technologies and others.
+ * Copyright (c) 2009, 2013 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -96,7 +96,7 @@ public class TaskEditorPerformanceTest extends PerformanceTestCase {
 	}
 
 	public void testOpenNewBugzillaTask() throws Exception {
-		TaskRepository repository = new TaskRepository("bugzilla", "http://mylyn.eclipse.org/bugs34/");
+		TaskRepository repository = new TaskRepository("bugzilla", "http://mylyn.org/bugzilla-4.4");
 		TasksUi.getRepositoryManager().addRepository(repository);
 		TasksUi.getRepositoryConnector(repository.getConnectorKind()).updateRepositoryConfiguration(repository,
 				new NullProgressMonitor());
